5529d5d8d3e86da6bdd6763d26a4dbc34c39f721
Vinícius Sena <senavinicius01@gmail.com>
2023-07-27 23:26:46 -0300

    filters fineshed

--- src/components/Filters/index.tsx
+++ src/components/Filters/index.tsx
@@ -3,13 +3,28 @@ import styles from './Filters.module.scss';
 import regions from '../../assets/Data/regions.json';
 import {IStyle} from "../../interface/IStyle";
 
-export default function Filters(props: IStyle) {
+interface filters extends  IStyle{
+    search: React.Dispatch<string>,
+    value: string,
+    filter: React.Dispatch<string>,
+    valueFilter: string
+}
+
+export default function Filters(props: filters) {
     return (
         <section className={styles.container}>
             <div className={styles.container__div}>
-                <input type="text" name="search" id="search" placeholder="Search for a country..." className={`${props.darkMode ? styles.container__div_input_dark : styles.container__div_input}`} />
+                <input
+                    type="text"
+                    name="search"
+                    id="search"
+                    placeholder="Search for a country..."
+                    value={props.value}
+                    onChange={(event: any) => props.search(event.target.value)}
+                    className={`${props.darkMode ? styles.container__div_input_dark : styles.container__div_input}`}
+                />
                 <div>
-                    <select className={`${props.darkMode ? styles.select_dark : "chakra - select Filters_container__div_select__7Juk4 css-1ik61og"}`} id="select" name="select" defaultValue="">
+                    <select className={`${props.darkMode ? styles.select_dark : "chakra - select Filters_container__div_select__7Juk4 css-1ik61og"}`} id="select" name="select" value={props.valueFilter} onChange={(event) => props.filter(event.target.value)}>
                         <option value="" hidden={true}>Filter by region</option>
                         {regions.region.map((region: string) => (
                             <option key={region} value={region}>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,25 +1,44 @@
 import Header from "../../components/Header";
 import Filters from "../../components/Filters";
-import axios, {AxiosError, AxiosResponse} from "axios";
+import {AxiosError, AxiosResponse} from "axios";
 import React, {useEffect, useState} from "react";
 import {ICountries} from "../../interface/ICountries";
 import Cards from "../../components/Cards";
 import styles from "./Home.module.scss";
+import http from "../../environments/environment"
 
 export default function Home( ){
     const [countries, setCountries] = useState<ICountries[]>([]);
     const [darkMode, setDarkMode] = useState(false)
+    const [search, setSearch] = useState('')
+    const [filter, setFilter] = useState('')
 
     async function getAllCountries() {
-        await axios.get(`https://restcountries.com/v3.1/all?fields=name,flags,capital,population,region,subregion,tld,currencies,languages`)
-            .then((res: AxiosResponse<any, any>) => {
+        await http.get(`all?fields=name,flags,capital,population,region,subregion,tld,currencies,languages`)
+            .then((res: AxiosResponse<ICountries[]>) => {
                 setCountries(res.data)})
             .catch((error: AxiosError) => console.log(error))
+    }
+
+    async function getCountriesForRegion(){
+        if(filter !== ''){
+            await http.get(`region/${filter.toLowerCase()}`).then((res: AxiosResponse<ICountries[]>) => {
+                setCountries(res.data)
+            } ).catch((error: AxiosError) => console.log(error))
+        }
     }
 
     useEffect(() => {
         getAllCountries();
     }, [])
+
+    useEffect(() => {
+        getCountriesForRegion();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [filter]);
+
+    const lowerSearch: string = search.toLowerCase()
+    const countriesFilter = countries.filter((element) => element.name.common.toLowerCase().includes(lowerSearch))
 
     return(
         <>
@@ -30,9 +49,14 @@ export default function Home( ){
             <main className={`${darkMode ? styles.dark_mode_home : ""}`}>
                 <Filters
                     darkMode={darkMode}
-                    setDarkMode={null}/>
+                    setDarkMode={null}
+                    search={setSearch}
+                    value={search}
+                    filter={setFilter}
+                    valueFilter={filter}
+                />
                 <Cards
-                    data={countries}
+                    data={countriesFilter}
                     darkMode={darkMode}
                     setDarkMode={null}
                 />
